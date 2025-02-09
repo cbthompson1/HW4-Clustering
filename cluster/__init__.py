e7cc93c70fe88c4e0fd9dce0e37ff4c1b2aaec37
cbthompson1 <cbthompson@hmc.edu>
2025-02-08 22:21:42 -0800

    update init

--- cluster/__init__.py
+++ cluster/__init__.py
@@ -1,8 +1,10 @@
+"""
+Homework 4: KMeans Clustering
+Chris Thompson
+"""
 from .kmeans import KMeans
 from .silhouette import Silhouette
 from .utils import (
         make_clusters, 
         plot_clusters,
         plot_multipanel)
-
-"""Homework 4: KMeans Clustering"""
